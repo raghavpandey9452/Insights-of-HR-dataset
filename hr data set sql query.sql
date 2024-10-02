1428af3c4b90247f273e2a32a973facb98d4a587
raghavpandey9452 <85411897+raghavpandey9452@users.noreply.github.com>
2024-10-02 22:27:02 +0530

    Add files via upload

---
+++ hr data set sql query.sql
@@ -0,0 +1,34 @@
+use rp;
+
+
+# what are company's top 5 recruitment sources
+select count(RecruitmentSource),RecruitmentSource 
+  FROM hrdataset_v14 group by RecruitmentSource order by count(RecruitmentSource) desc limit 5; 
+  
+  
+# Department which has terminated most employees for a cause
+select  count(EmpID),Department
+  FROM hrdataset_v14 where EmploymentStatus='Terminated for Cause' group by Department ;
+  
+  
+#department which has most employee satisfaction
+select avg(EmpSatisfaction),Department  FROM hrdataset_v14 group by Department;
+
+
+# which manager has most employee satisfaction and no. of person he manages working under him
+select avg(EmpSatisfaction),count(EmpID)as manages_people ,ManagerName  FROM hrdataset_v14 group by ManagerName order by avg(EmpSatisfaction) desc;
+
+
+#top salary in each department
+select max(salary), Department from hrdataset_v14 group by Department ;
+
+
+#Least salary in each department
+select min(salary), Department from hrdataset_v14 group by Department ;
+
+
+# Reason for leaving the company
+select count(EmpID),TermReason from hrdataset_v14 
+group by TermReason order by count(EmpID) desc ;
+
+

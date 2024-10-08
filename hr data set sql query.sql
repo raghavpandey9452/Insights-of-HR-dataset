d2ff06186cd6bb8b3b254376ec08152ca37b5e69
raghavpandey9452 <85411897+raghavpandey9452@users.noreply.github.com>
2024-10-08 19:30:18 +0530

    Add files via upload

--- hr data set sql query.sql
+++ hr data set sql query.sql
@@ -26,14 +26,49 @@ select max(salary), Department from hrdataset_v14 group by Department ;
 #Least salary in each department
 select min(salary), Department from hrdataset_v14 group by Department ;
 
+#Employee who have salary more than average salary
+
+
 
 # Reason for leaving the company
 select count(EmpID),TermReason from hrdataset_v14 
 group by TermReason order by count(EmpID) desc ;
 
+#Employee who have salary more than average salary.
+ 
+ select salary,Nameep from hrdataset_v14 where salary>(select avg(Salary) from hrdataset_v14) order by salary ;
+
+
+
+
 # Number of Employees in each department
 select count(EmpID), Department from hrdataset_v14 
 group by Department order by count(EmpID) desc ;
+
+# Get all data from table along with max salary of that department(use of window function)
+select * ,
+max(Salary) over(partition by Department ) as max_salary 
+ from hrdataset_v14;
+ 
+ #extract employ of each department who joined company before to after
+ select Nameep,EmpID,Department ,
+ row_number() over(partition by Department order by EmpID) as loyal_employees
+ from hrdataset_v14;
+ 
+ # Fetch top 3 employees from each department according to salary
+ select * from(
+ select Nameep,EmpID,Department ,Salary,
+ rank() over(partition by Department order by salary desc ) as most_earning
+ from hrdataset_v14) x
+ where x.most_earning<4;
+ 
+
+ 
+
+ 
+
+
+
 
 
 
